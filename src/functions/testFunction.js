edaff500444f5d0be4c3e177c446900ef2c2e8fe
ijk <ijkten@no-email.com>
2020-04-27 19:22:43 -0400

    template for aws with sls using lambdas and dynamodb

---
+++ src/functions/testFunction.js
@@ -0,0 +1,43 @@
+'use strict';
+
+module.exports.testFunction = async (event, context) => {
+  //For a post method
+  // const body = JSON.parse(event.body)
+  // const username = body.username
+  // const password = body.password
+  // const newUserParams = {
+  //   TableName: process.env.DYNAMODB_USER_TABLE,
+  //   Item: {
+  //     pk: username,
+  //     password: bcrypt.hashSync(password, 10)
+  //   }
+  // }
+  // try {
+  //   const dynamodb = new AWS.DynamoDB.DocumentClient()
+  //   const putResult = await dynamodb.put(newUserParams).promise()
+  //   return {
+  //     statusCode: 201,
+  //     headers: {
+  //       'Access-Control-Allow-Origin': '*',
+  //       'Access-Control-Allow-Credentials': true,
+  //       'Access-Control-Allow-Headers': 'Authorization'
+  //     }
+  //   }
+  // } catch(putError) {
+  //   console.log('There was an error putting the new item')
+  //   console.log('putError', putError)
+  //   console.log('newUserParams', newUserParams)
+  //   return new Error('There was an error putting the new item')
+  // }
+
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Go Serverless v1.0! Your function executed successfully!'
+    }),
+  };
+
+  return response;
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
