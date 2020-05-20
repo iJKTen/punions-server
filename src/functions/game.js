765a6d4bba0516a4ac5c579827587e44cc357e44
ijk <ijkten@no-email.com>
2020-05-19 22:55:28 -0400

    testing REST API with Websocket

--- src/functions/game.js
+++ src/functions/game.js
@@ -4,22 +4,28 @@ const db = require('../db/game');
 const utilities = require('../utilities/index');
 
 module.exports.createGame = async (event, context) => {
-  
   try {
     const data = await db.createGame();
     return utilities.success(data);
   } catch (err) {
     return utilities.error(err);
   }
-
 };
 
 module.exports.addPlayerToGame = async (event, context) => {
-
   try {
     
   } catch (err) {
     return utilities.error(err);
   }
+}
 
+module.exports.playerLeftGame = async (event, context) => {
+  return utilities.success('player left game');
+}
+
+module.exports.sendMessage = async (event, context) => {
+  console.log(event);
+  console.log(context);
+  return utilities.success(event);
 }
---
+++ src/functions/websocket.js
@@ -0,0 +1,8 @@
+'use strict'; 
+const AWS = require("aws-sdk");
+
+module.exports.connectionManager = async (event, context) => {
+  return {
+    statusCode: 200
+  };
+}
